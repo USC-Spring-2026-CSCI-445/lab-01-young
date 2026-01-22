c801ad80ff89801f4ff5316c334316bca0143890
Chenhongyu Shi <stevenshi030109@gmail.com>
2026-01-22 10:39:36 -0800

    movement

--- scripts/lab1_starter.py
+++ scripts/lab1_starter.py
@@ -22,6 +22,13 @@ class TurtlebotController:
         move_cmd = Twist()
         ######### Your code starts here #########
         # stop the TurtleBot
+        move_cmd.linear.x = 0.0
+        move_cmd.linear.y = 0.0
+        move_cmd.linear.z = 0.0
+
+        move_cmd.angular.x = 0.0
+        move_cmd.angular.y = 0.0
+        move_cmd.angular.z = 0.0
 
         ######### Your code ends here #########
         self.publish_twist(move_cmd, 1)  # Stop for 1 second
@@ -31,7 +38,8 @@ class TurtlebotController:
         move_cmd = Twist()
         ######### Your code starts here #########
         # move forward at a constant speed
-
+        move_cmd.linear.x = 0.2
+        move_cmd.angular.z = 0.0
         ######### Your code ends here #########
         self.publish_twist(move_cmd, 10)  # Move forward for 10 seconds
 
@@ -40,6 +48,8 @@ class TurtlebotController:
         move_cmd = Twist()
         ######### Your code starts here #########
         # move backward at a constant speed
+        move_cmd.linear.x = -0.2 
+        move_cmd.angular.z = 0.0
 
         ######### Your code ends here #########
         self.publish_twist(move_cmd, 10)  # Move backward for 10 seconds
@@ -49,6 +59,8 @@ class TurtlebotController:
         move_cmd = Twist()
         ######### Your code starts here #########
         # turn left in place
+        move_cmd.linear.x = 0.0
+        move_cmd.angular.z = 0.5
 
         ######### Your code ends here #########
         self.publish_twist(move_cmd, 5)  # Turn left for 5 seconds
@@ -58,6 +70,8 @@ class TurtlebotController:
         move_cmd = Twist()
         ######### Your code starts here #########
         # turn right in place 
+        move_cmd.linear.x = 0.0
+        move_cmd.angular.z = -0.5
 
         ######### Your code ends here #########
         self.publish_twist(move_cmd, 5)  # Turn right for 5 seconds
